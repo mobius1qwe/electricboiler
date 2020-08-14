00f03fbccf5b0f6682f4e73885bd83b3eb7e9fcb
mobius1qwe <26689802+mobius1qwe@users.noreply.github.com>
2020-08-14 02:10:46 -0300

    mod: electricboiler Changes: - Fixed issue #009 added settings to change power output of the boiler. - Ported mod to Factorio v1.0

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -1,12 +1,11 @@
 local entityboiler = table.deepcopy(data.raw["boiler"]["boiler"])
 local entityoverride = {
   name = "electric-boiler",
-  energy_consumption = "1.0MW",
+  energy_consumption = settings.startup["efficiency"].value .. "MW",
   minable = {hardness = 0.2, mining_time = 0.5, result = "electric-boiler"},
   energy_source =
   {
       type = "electric",
-      effectivity = 0.6,
       input_priority = "secondary",
       usage_priority = "secondary-input",
       light_flicker = {
